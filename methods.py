968dd43df259dfcb1d3ee94bf332c9c172bbea41
Victor <vicbotce@inf.upv.es>
2022-06-18 01:47:16 +0200

    lat lon corrected 2

--- methods.py
+++ methods.py
@@ -8,7 +8,7 @@ def XY_To_LatLon(x,y):
     P = pyproj.Proj(proj='utm', zone=30, ellps='WGS84', preserve_units=True)
     G = pyproj.Geod(ellps='WGS84')
 
-    P(x,y,inverse=True)    
+    (lat, lon) = P(x,y,inverse=True)    
     return (lat, lon)
 
 
