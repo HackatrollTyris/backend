0a02f759a362445ff3a32cdfca1cf83199f2b61f
Victor <vicbotce@inf.upv.es>
2022-06-18 03:59:52 +0200

    flask add method get

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from crypt import methods
 from flask import Flask
 from methods import *
 
 app = Flask(__name__)
 
-@app.route("/shop_ids")
+@app.route("/shop_ids", methods=['GET'])
 def get_shops():
     return get_shops_ids()
 
