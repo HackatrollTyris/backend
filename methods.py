c35268c90e3aeaba919cd6e4352319ca25bcd8df
Victor <vicbotce@inf.upv.es>
2022-06-18 02:07:24 +0200

    get districts from api

--- methods.py
+++ methods.py
@@ -2,6 +2,7 @@ import pyproj
 import math
 import requests
 import json
+import pandas as pd
 
 
 def XY_To_LatLon(x,y):
@@ -11,6 +12,22 @@ def XY_To_LatLon(x,y):
     (lat, lon) = P(x,y,inverse=True)    
     return (lat, lon)
 
+def getDistricts():
+    districts_pd = pd.DataFrame(columns=['district_id', 'name', 'coordinates'])
+    open_data_url = 'https://geoportal.valencia.es/apps/OpenData/UrbanismoEInfraestructuras/DISTRITOS.json'
+    resp = requests.get(url = open_data_url)
+    data = resp.json()
+    for district in data['features']:
+        name = district['properties']['nombre']
+        district_id = district['properties']['coddistrit']
+        coordinates = district['geometry']['coordinates']
+        districts_pd = districts_pd.append({'district_id' : district_id, 
+                                            'name' : name, 
+                                            'coordinates' : coordinates[0]}, ignore_index=True)
+    
+    
+    districts_pd.to_csv('data/districts.csv', index=False)
+    
 
 
-
+getDistricts()
